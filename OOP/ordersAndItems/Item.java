40515369779fd693f596e3b7fe2a8e81881d06f4
Chris Frederick <chrisfrederick2008@gmail.com>
2021-11-30 11:12:54 -0600

    Baristas Challenge

--- OOP/ordersAndItems/Item.java
+++ OOP/ordersAndItems/Item.java
@@ -1,6 +1,29 @@
 package ordersAndItems;
 
 public class Item {
-    String name;
-    double price;
+    private String name;
+    private double price;
+
+    public Item(){}
+
+    public Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public double getPrice(){
+        return this.price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
--- OOP/ordersAndItems/Main.java
+++ OOP/ordersAndItems/Main.java
@@ -3,56 +3,31 @@ package ordersAndItems;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
+public static void main(String[] args) {
+
+    // Menu items
+    Item item1 = new Item("cappucino", 5.5);
+    Item item2 = new Item("Mocha", 6.5);
+    Item item3 = new Item("Latte", 4.5);
+    Item item4 = new Item("Drip Coffee", 5.0);
+
+    Order order1 = new Order();
+    Order order2 = new Order();
+    Order order3 = new Order("Anna");
+    Order order4 = new Order("Emilie");
+    Order order5 = new Order("Coco");
+    order1.addItem(item1);
+    order1.addItem(item4);
+    System.out.println(order1.getStatusMessage());
+    order1.setReady(true);
+    System.out.println(order1.getStatusMessage());
+    System.out.println(order1.getOrderTotal());
+
+    order3.addItem(item1);
+    order3.addItem(item4);
+    order3.addItem(item2);
+    order3.display();
         
 
-
-        // Menu items
-        Item item1 = new Item();
-        item1.name = "mocha";
-        item1.price = 3.5;
-
-        Item item2 = new Item();
-        item2.name = "latte";
-        item2.price = 4.5;
-
-        Item item3 = new Item();
-        item3.name = "drip coffee";
-        item3.price = 5.5;
-
-        Item item4 = new Item();
-        item4.name = "capuccino";
-        item4.price = 2.5;
-    
-        // Order variables -- order1, order2 etc.
-        Order order1 = new Order();
-        order1.name = "Chris";
-
-        Order order2 = new Order();
-        order2.name = "Nysha";
-        order2.items.add(item1);
-        order2.total = item1.price;
-        order2.ready = true;
-
-        Order order3 = new Order();
-        order3.name = "Harry";
-        order3.items.add(item4);
-        order3.items.add(item2);
-        order3.items.add(item2);
-        order3.total = item1.price;
-        order3.total = item2.price*2;
-
-        Order order4 = new Order();
-        order4.name = "Murphy";
-        order4.items.add(item2);
-        order4.total = item2.price;
-        order4.ready = true;
-    
-        // Application Simulations
-        System.out.println("Order is on "+ order1);
-        // Use this example code to test various orders' updates
-        System.out.printf("Name: %s\n", order1.name);
-        System.out.printf("Total: %s\n", order1.total);
-        System.out.printf("Ready: %s\n", order1.ready);
     }
 }
