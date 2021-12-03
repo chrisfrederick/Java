4a19f17d19cf05c834257be6a720cf71d0e28fda
Chris <chrisfrederick2008@gmail.com>
2021-12-03 13:58:07 -0600

    Daikichi Path Variables

--- Spring/src/main/java/com/example/spring/DaikichiController.java
+++ Spring/src/main/java/com/example/spring/DaikichiController.java
@@ -1,10 +1,7 @@
 package com.example.spring;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
@@ -24,6 +21,23 @@ public class DaikichiController {
     @GetMapping("daikichi/tomorrow")
     public String tomorrow(){
         return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+    }
+
+    @GetMapping("daikichi/{travel}/{kyoto}")
+    public String travel(@PathVariable("travel") String travel, @PathVariable("kyoto") String kyoto){
+        return "Congratulations! You will soon travel to "+kyoto;
+    }
+
+    @GetMapping("daikichi/path/{lotto}/{number}")
+    public String lotto(@PathVariable("loto") String loto, @PathVariable("number") Integer number){
+//        int num = Integer.parseInt(number);
+        if(number%2 == 0){
+            return "You will takes a grand journey in the near future, but be weary of tempting offers";
+        }else if(number%3==0){
+            return "You have enjoyed the fruits of your labor but now is a great time to spend with family and friends";
+        }else {
+            return "Not a valid number";
+        }
     }
 
 }
